c8c003c6d5478ff3238995489f963c18f8d34566
Aman Ahmed <amanahmedbarca@gmail.com>
2024-07-06 17:28:08 +0530

    new commit

---
+++ Bubble Sort.py
@@ -0,0 +1,16 @@
+def BubbleSort(list):
+
+    n=len(list)
+
+    for i in range(n-1,0,-1):
+        for j in range(i):
+            if list[j]> list[j+1]:
+                list[j],list[j+1]=list[j+1],list[j]
+
+ 
+    return list
+
+print('new')
+
+list=[5,8,2,9,1,6]
+print(BubbleSort(list))
