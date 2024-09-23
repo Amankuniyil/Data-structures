e54ba28cfcc1307ba47ba5028fb31acc72483670
Aman Ahmed <amanahmedbarca@gmail.com>
2024-09-23 22:53:59 +0530

    Added double linked list

---
+++ Data Structures/doubly linkedList.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+        new_node.prev = last
+
+    def prepend(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        self.head.prev = new_node
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete(self, key):
+        temp = self.head
+        if temp and temp.data == key:
+            self.head = temp.next
+            if self.head:
+                self.head.prev = None
+            return
+        while temp and temp.data != key:
+            temp = temp.next
+        if not temp:
+            return
+        if temp.next:
+            temp.next.prev = temp.prev
+        if temp.prev:
+            temp.prev.next = temp.next
+
+    def display(self):
+        elems = []
+        curr = self.head
+        while curr:
+            elems.append(curr.data)
+            curr = curr.next
+        return elems
